d4293a8b03fc00c6c90f6c963fdca483327f6fbb
Kj£99shjKu34qq <frare@publicisgroupe.net>
2023-06-01 19:01:14 +0200

    feat: setup aws enviroment for main-dev

--- infrastructure-aws/terraform/modules/iam/main.tf
+++ infrastructure-aws/terraform/modules/iam/main.tf
@@ -77,7 +77,7 @@ resource "aws_iam_policy_attachment" "ec2_policy_role" {
 }
 
 #Attach role to an instance profile
-resource "aws_iam_instance_profile" "ec2_profile_dev" {
-  name = "ec2_profile_dev"
+resource "aws_iam_instance_profile" "ec2_profile_main_dev" {
+  name = "ec2_profile_main_dev"
   role = aws_iam_role.ec2_role_dev.name  
 }
